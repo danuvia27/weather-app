b94f7c6b6d0546968bf208760648e12e137eda0f
danuvia27 <87292336+danuvia27@users.noreply.github.com>
2021-08-19 15:13:17 -0700

    Moved project to Github

---
+++ script.js
@@ -0,0 +1,58 @@
+//current time/date
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayIndex = date.getDay();
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+
+  return `${days[dayIndex]} ${hours}:${minutes}`;
+}
+let dateElement = document.querySelector("#date-time");
+let currentTime = new Date();
+
+dateElement.innerHTML = formatDate(currentTime);
+
+//search weather engine
+function cityTemprature(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temprature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+
+function searchCity(city) {
+  let apiKey = "500ad9e053abf399ecb2669d220f8e83";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(cityTemprature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-text-input").value;
+  searchCity(city);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("San Diego");
